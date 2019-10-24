c9a17d2c0e243d93ffad8666894e66337a248b9d
ttanja <sineenad.junmookda@mail.kmutt.ac.th>
2019-10-24 20:09:32 +0700

    edit Login.js

--- src/Login.js
+++ src/Login.js
@@ -13,6 +13,7 @@ import {
 import React, { Component } from "react";
 import "./App.css";
 import logo2 from "./LOGO.png";
+import background from "./bglogin-new.png";
 import { NavLink } from "react-router-dom";
 const { Header, Footer, Sider, Content } = Layout;
 
@@ -28,66 +29,66 @@ class Login extends React.Component {
 
   render() {
     return (
-      <Layout>
-        <div className="header">
-          <Row className="row1">
-            <Col span={24}></Col>
-          </Row>
-          <Row className="row2">
-            <Col span={6}></Col>
-            <Col span={12}>
-              <img src={logo2} style={{ height: 100 }} />
-            </Col>
-            <Col span={6}></Col>
-          </Row>
-          <Row className="row3">
-            <Col span={24}></Col>
-          </Row>
-          <Row className="row4">
-            <Col span={6}></Col>
-            <Col span={12}>
-              <div className="login-box">
-                <Input
-                  className={"usernameInput"}
-                  prefix={
-                    <Icon type="user" style={{ color: "rgba(0,0,0,.25)" }} />
-                  }
-                  placeholder="Username"
-                />
-                <br></br>
-                <Input
-                  className={"passwordInput"}
-                  prefix={
-                    <Icon type="lock" style={{ color: "rgba(0,0,0,.25)" }} />
-                  }
-                  type="password"
-                  placeholder="Password"
-                />
-                <br></br>
-                <Checkbox className={"remember"}>Remember me</Checkbox>
-                <a className="login-form-forgot" href="">
-                  Forgot password
+        <Layout>
+          <div className="header">
+            <Row className="row1">
+              <Col span={24}></Col>
+            </Row>
+            <Row className="row2">
+              <Col span={6}></Col>
+              <Col span={12}>
+                <img src={logo2} style={{ height: 100 }} />
+              </Col>
+              <Col span={6}></Col>
+            </Row>
+            <Row className="row3">
+              <Col span={24}></Col>
+            </Row>
+            <Row className="row4">
+              <Col span={8}></Col>
+              <Col span={8}>
+                <div className="login-box">
+                  <Input
+                    className={"usernameInput"}
+                    prefix={
+                      <Icon type="user" style={{ color: "rgba(0,0,0,.25)" }} />
+                    }
+                    placeholder="Username"
+                  />
+                  <br></br>
+                  <Input
+                    className={"passwordInput"}
+                    prefix={
+                      <Icon type="lock" style={{ color: "rgba(0,0,0,.25)" }} />
+                    }
+                    type="password"
+                    placeholder="Password"
+                  />
+                  <br></br>
+                  <Checkbox className={"remember"}>Remember me</Checkbox>
+                  <a className="login-form-forgot" href="">
+                    Forgot password
                 </a>
-                <br></br>
-                <Button type="primary" className="login-form-button">
-                  Log in
+                  <br></br>
+                  <Button type="primary" className="login-form-button">
+                    Log in
                 </Button>
-                <br></br>
-                <div className={"register"}>
-                  Or
+                  <br></br>
+                  <div className={"register"}>
+                    Or
                   <NavLink to="/register">
-                    <a className={"register-link"}>register now!</a>
-                  </NavLink>
+                      <a className={"register-link"}>register now!</a>
+                    </NavLink>
+                  </div>
                 </div>
-              </div>
-            </Col>
-            <Col span={6}></Col>
-          </Row>
-          <Row className="row5">
-            <Col span={24}></Col>
-          </Row>
-        </div>
-      </Layout>
+              </Col>
+              <Col span={8}></Col>
+            </Row>
+            <Row className="row5">
+              <Col span={24}></Col>
+            </Row>
+          </div>
+        </Layout>
     );
   }
 }
